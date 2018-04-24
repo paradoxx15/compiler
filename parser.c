15231fc1485584b88e8188fd88ca8279155ff837
Ben Faria <benfaria_96@yahoo.com>
2018-04-24 16:46:26 -0400

    fixed print

--- parser.c
+++ parser.c
@@ -1109,8 +1109,11 @@ int main(int argc, char **argv)
         printCode();
     if (error == 0)
     {
-        printf("\n-------------------------------------------\n");
-        printf("PROGRAM INPUT/OUTPUT:\n");
+        if (!vmPrint)
+        {
+            printf("\n-------------------------------------------\n");
+            printf("PROGRAM INPUT/OUTPUT:\n");
+        }
         vm();
         printf("\nFinished execution. Exiting...");
     }
